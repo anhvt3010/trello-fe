a6833bb1b233ec48caeb797bd39fcd8d702448a6
Vu Tuan Anh <anh.vt.3010@gmail.com>
2024-02-26 15:42:59 +0700

    config theming

--- .history/src/main_20240226153953.jsx
+++ .history/src/main_20240226153953.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@emotion/react'
+import ThemeProvider from '@mui/material/the'
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
