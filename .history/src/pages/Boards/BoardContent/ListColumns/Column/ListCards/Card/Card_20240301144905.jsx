378630ad3b4a208e74b56ed798b9c77ac18d4653
Vu Tuan Anh <anh.vt.3010@gmail.com>
2024-03-01 16:11:44 +0700

    add mock data, sort data

---
+++ .history/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card_20240301144905.jsx
@@ -0,0 +1,33 @@
+import { Attachment, Comment, Group } from '@mui/icons-material'
+import { Button, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import { Card as MuiCard } from '@mui/material'
+
+function Card({ card }) {
+  return (
+    <MuiCard sx={{
+      cursor: 'pointer',
+      boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+      overflow: 'unset'
+    }}>
+      {card?.cover && <CardMedia sx={{ height: 140 }} image={card.cover} />}
+
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>{card?.title}</Typography>
+      </CardContent>
+
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        {!!card?.memberIds?.length &&
+          <Button size="small" startIcon={<Group />}>{card.memberIds.length}</Button>
+        }
+
+        {!!card?.comments?.length &&
+          <Button size="small" startIcon={<Group />}></Button>
+        }
+        
+        <Button size="small" startIcon={<Attachment />}>10</Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
---
+++ .history/src/pages/Boards/BoardContent/ListColumns/ListColumns_20240301143528.jsx
@@ -0,0 +1,44 @@
+import Box from '@mui/material/Box'
+import Column from './Column/Column'
+import { Button } from '@mui/material'
+import { NoteAdd } from '@mui/icons-material'
+
+function ListColumns({ columns }) {
+  return (
+    <Box sx={{
+      bgcolor: 'inherit',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      overflowX: 'auto',
+      overflowY: 'hidden',
+      '&::-webkit-scrollbar-track': { m: 2 }
+    }}>
+      {columns?.map(column => (        <Column key={column._id} />
+      )}
+
+      {/* Box Add new column CTA */}
+      <Box sx={{
+        minWidth: '200px',
+        maxWidth: '200px',
+        mx: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        bgcolor: '#ffffff3d'
+      }}>
+        <Button
+          sx={{
+            color: 'white',
+            width: '100%',
+            justifyContent: 'flex-start',
+            pl: 2.5,
+            py: 1
+          }}
+          startIcon={<NoteAdd />}
+        >Add new column</Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default ListColumns
