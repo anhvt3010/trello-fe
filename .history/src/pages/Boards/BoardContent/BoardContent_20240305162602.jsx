8c68c36d22966e8bd8005833e8cd3434519fbac6
Vu Tuan Anh <anh.vt.3010@gmail.com>
2024-03-05 18:24:25 +0700

    drag drop card in colum, preview drag drop column, card

---
+++ .history/src/pages/Boards/BoardContent/BoardContent_20240305162602.jsx
@@ -0,0 +1,82 @@
+import Box from '@mui/material/Box'
+import ListColumns from './ListColumns/ListColumns'
+import { mapOrder } from '~/utils/sorts'
+import { DndContext, MouseSensor, PointerSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
+  CARD: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN'
+}
+
+function BoardContent({ board }) {
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: { distance: 10 }
+  })
+
+  // con chuột di chuyển 10px để kích hoạt event, tránh click => kích hoạt
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  // nhấn giữ 250ms
+  // tolerance là dung sai của cảm ứng (tay, bút cảm ứng, ... => khác nhau)
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 250,
+      tolerance: 5
+    }
+  })
+
+  // const sensors = useSensors(pointerSensor)
+  // ưu tiên sử dụng, tránh bug trên mobile khi cảm ứng chạm
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragStart = (event) => {
+
+  }
+
+  const handleDragEnd = (event) => {
+    // active: vị trí cũ, over: Vị trí kéo đến
+    const { active, over } = event
+
+    // handle khi kéo ra ngoài
+    if (!over) return
+
+    // Vị trí sau kéo thả khác vị trí ban đầu
+    if (active.id !== over.id) {
+      // Lấy vị trí cũ từ active
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lấy vị trí mới từ over
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      // Cập nhật columnOrderIds: Sắp xếp lại mảng columns
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
+
+  return (
+    <DndContext
+      sensors={sensors}>
+      onDragStart = {handleDragStart}
+      onDragEnd={handleDragEnd}
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
+  )
+}
+
+export default BoardContent
---
+++ .history/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card_20240305160443.jsx
@@ -0,0 +1,57 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { Attachment, Comment, Group } from '@mui/icons-material'
+import { Button, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import { Card as MuiCard } from '@mui/material'
+
+function Card({ card }) {
+  // drag and drop
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: card._id,
+    data: { ...card }
+  })
+  const dndKitCardStyles = {
+    // touchAction: 'none',
+    // sử dụng CSS.Transform như trong doc sẽ bị lỗi stretch
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
+  const shouldShowCardAction = () => {
+    return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
+  }
+
+  return (
+    <MuiCard
+      ref={setNodeRef} style={dndKitCardStyles} {...attributes} {...listeners}
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+        overflow: 'unset'
+      }}>
+      {card?.cover && <CardMedia sx={{ height: 140 }} image={card.cover} />}
+
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>{card?.title}</Typography>
+      </CardContent>
+
+      {shouldShowCardAction() &&
+        <CardActions sx={{ p: '0 4px 8px 4px' }}>
+          {!!card?.memberIds?.length &&
+            <Button size="small" startIcon={<Group />}>{card.memberIds.length}</Button>
+          }
+
+          {!!card?.comments?.length &&
+            <Button size="small" startIcon={<Comment />}>{card.comments.length}</Button>
+          }
+
+          {!!card?.attachments?.length &&
+            <Button size="small" startIcon={<Attachment />}>{card.attachments.length}</Button>
+          }
+        </CardActions>
+      }
+
+    </MuiCard>
+  )
+}
+
+export default Card
